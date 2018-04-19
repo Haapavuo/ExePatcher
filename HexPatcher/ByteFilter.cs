3d1e7bb1ffa99ce556fb9a88b7401c8f7f5dda68
Jeri Haapavuo <jeri.haapavuo@gmail.com>
2018-04-19 19:10:50 +0300

    Added the solution. Added "config.cfg" to gitignore.

---
+++ HexPatcher/ByteFilter.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HexPatcher
+{
+    /// <summary>
+    /// A byte array that may contain wildcard bytes instead of data.
+    /// </summary>
+    public class ByteFilter
+    {
+        /// <summary>
+        /// The data bytes.
+        /// </summary>
+        public byte[] Bytes { get; set; }
+
+        /// <summary>
+        /// Length of the data.
+        /// </summary>
+        public int Length { get { return Bytes.Length; } }
+
+        private List<long> wildcardIndexes = new List<long>();
+
+        /// <summary>
+        /// Constructs a new ByteFilter of the given hex string.
+        /// The string may contain spaces and wildcards '*'.
+        /// </summary>
+        public ByteFilter(string hexString)
+        {
+            hexString = hexString.Replace(" ", "");
+            Bytes = StringToByteArray(hexString);
+
+            // Get all wildcards from the hex string
+            for (int i = hexString.IndexOf('*'); i > -1; i = hexString.IndexOf('*', i + 1))
+            {
+                wildcardIndexes.Add(i / 2);
+            }
+
+            // Remove possible duplicates
+            wildcardIndexes = wildcardIndexes.Distinct().ToList();
+        }
+
+        /// <summary>
+        /// Returns true if the given index contains a wildcard.
+        /// </summary>
+        public bool IsWildcardIndex(long index)
+        {
+            return wildcardIndexes.Contains(index);
+        }
+
+        /// <summary>
+        /// Converts a hex string to a byte array.
+        /// </summary>
+        private static byte[] StringToByteArray(string hex)
+        {
+            if (hex.Length % 2 == 1)
+            {
+                throw new Exception("The binary key cannot have an odd number of digits");
+            }
+
+            byte[] arr = new byte[hex.Length >> 1];
+
+            for (int i = 0; i < hex.Length >> 1; ++i)
+            {
+                // Two hex characters represent one byte
+                arr[i] = (byte)((GetHexValue(hex[i << 1]) << 4) + (GetHexValue(hex[(i << 1) + 1])));
+            }
+
+            return arr;
+        }
+
+        /// <summary>
+        /// Converts an encoded hex character into a byte value.
+        /// </summary>
+        private static int GetHexValue(char hex)
+        {
+            if (hex == '*')
+            {
+                return 0;
+            }
+
+            int val = hex;
+
+            //For uppercase A-F letters:
+            return val - (val < 58 ? 48 : 55);
+
+            //For lowercase a-f letters:
+            //return val - (val < 58 ? 48 : 87);
+
+            //Or the two combined, but a bit slower:
+            //return val - (val < 58 ? 48 : (val < 97 ? 55 : 87));
+        }
+    }
+}
---
+++ HexPatcher/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.IO;
+
+namespace HexPatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configFilePath = "config.cfg";
+            if (args.Length > 0)
+            {
+                configFilePath = args[0];
+            }
+
+            if (!File.Exists(configFilePath))
+            {
+                Console.WriteLine($"Configuration file '{configFilePath}' not found.");
+                Console.WriteLine($"Working directory path: {Directory.GetCurrentDirectory()}");
+            }
+            else
+            {
+                var config = File.ReadAllLines(configFilePath);
+
+                if (config.Length < 3)
+                {
+                    Console.WriteLine("Invalid config file.");
+                    Console.WriteLine("Please use the following format:");
+                    Console.WriteLine("BINARY FILE PATH");
+                    Console.WriteLine("ORIGINAL BYTES TO FIND");
+                    Console.WriteLine("MODIFIED BYTES TO REPLACE THE ORIGINAL WITH");
+                }
+                else
+                {
+                    var filePath = config[0];
+                    var originalHex = config[1];
+                    var modifiedHex = config[2];
+
+                    if (!File.Exists(filePath))
+                    {
+                        Console.WriteLine($"File '{filePath}' does not exist.");
+                    }
+                    else
+                    {
+                        var fileBytes = File.ReadAllBytes(filePath);
+                        var originalBytes = new ByteFilter(originalHex);
+                        var moddedBytes = new ByteFilter(modifiedHex);
+
+                        // Try to find the original bytes from the file
+                        var startIndex = GetStartIndex(fileBytes, originalBytes);
+
+                        if (startIndex < 0)
+                        {
+                            Console.WriteLine("Original bytes were not found.");
+                        }
+                        else
+                        {
+                            // Make a backup file
+                            if (SafeWriteBytesToFile(filePath + ".bak", fileBytes))
+                            {
+                                // Replace with the modified bytes & overwrite the original file
+                                WriteBytes(fileBytes, startIndex, moddedBytes);
+                                SafeWriteBytesToFile(filePath, fileBytes);
+                            }
+                        }
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Writes the given bytes to the given file path.
+        /// </summary>
+        /// <returns>True if success.</returns>
+        public static bool SafeWriteBytesToFile(string filePath, byte[] fileBytes)
+        {
+            try
+            {
+                File.WriteAllBytes(filePath, fileBytes);
+                Console.WriteLine("Bytes written to path: " + filePath);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Could not write bytes to file: " + e.Message);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Writes the bytes from the given ByteFilter into the given byte array
+        /// starting at the given index.
+        /// </summary>
+        public static void WriteBytes(byte[] writeTo, long startIndex, ByteFilter data)
+        {
+            for (long i = 0; i < data.Length; i++)
+            {
+                if (data.IsWildcardIndex(i))
+                {
+                    continue;
+                }
+                else
+                {
+                    writeTo[startIndex + i] = data.Bytes[i];
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns the start index of the first occurrence of the given ByteFilter.
+        /// </summary>
+        public static long GetStartIndex(byte[] fileBytes, ByteFilter findBytes)
+        {
+            long matchIndex = 0;
+            for (long i = 0; i < fileBytes.Length; i++)
+            {
+                if (findBytes.IsWildcardIndex(matchIndex) || fileBytes[i] == findBytes.Bytes[matchIndex])
+                {
+                    matchIndex++;
+                    if (matchIndex == findBytes.Length)
+                    {
+                        return i - matchIndex + 1;
+                    }
+                }
+                else
+                {
+                    matchIndex = 0;
+                }
+            }
+            return -1;
+        }
+    }
+}
